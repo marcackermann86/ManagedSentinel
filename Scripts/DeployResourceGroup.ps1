b629eb86e5e8a03a2521ba18d918476d28fa79dc
marcackermann86 <99517033+marcackermann86@users.noreply.github.com>
2023-11-18 21:10:49 -0500

    Create DeployResourceGroup.ps1

---
+++ Scripts/DeployResourceGroup.ps1
@@ -0,0 +1,24 @@
+param (
+    [Parameter(Mandatory=$true)]$OnboardingFile
+)
+
+#Adding AzSentinel module
+Uninstall-AzureRm
+Install-Module AzSentinelTools -Scope CurrentUser -AllowClobber -Force
+Import-Module AzSentinelTools
+
+$artifactName = "OnboardingFile"
+
+#Build the full path for the onboarding file
+$artifactPath = Join-Path $env:Pipeline_Workspace $artifactName 
+$onboardingFilePath = Join-Path $artifactPath $OnboardingFile
+
+$workspaces = Get-Content -Raw -Path $onboardingFilePath | ConvertFrom-Json
+
+foreach ($item in $workspaces.deployments){
+    Write-Host "Processing workspace $($item.workspace) ..."
+    $rg = Get-AzResourceGroup -ResourceGroupName $($item.resourceGroupName) -ErrorAction SilentlyContinue
+    if ($null -eq $rg) {
+        New-AzResourceGroup -Name $($item.resourceGroupName) -Location $($item.location) -Tag @{H="$($item.hnumber)"}
+    }
+}
