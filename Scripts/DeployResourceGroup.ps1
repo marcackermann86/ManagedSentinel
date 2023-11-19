3c3add909738897aa6e400753962bd5a3dab1b50
marcackermann86 <99517033+marcackermann86@users.noreply.github.com>
2023-11-18 23:50:18 -0500

    Update DeployResourceGroup.ps1

--- Scripts/DeployResourceGroup.ps1
+++ Scripts/DeployResourceGroup.ps1
@@ -1,19 +1,11 @@
 param (
-    [Parameter(Mandatory=$true)]$OnboardingFile
+    [Parameter(Mandatory=$true)]$Name
+    [Parameter(Mandatory=$true)]$Location
+    [Parameter(Mandatory=$true)]$Tag
 )
 
-$artifactName = "OnboardingFile"
-
-#Build the full path for the onboarding file
-$artifactPath = Join-Path $env:Pipeline_Workspace $artifactName 
-$onboardingFilePath = Join-Path $artifactPath $OnboardingFile
-
-$workspaces = Get-Content -Raw -Path $onboardingFilePath | ConvertFrom-Json
-
-foreach ($item in $workspaces.deployments){
-    Write-Host "Processing workspace $($item.workspace) ..."
-    $rg = Get-AzResourceGroup -ResourceGroupName $($item.resourcegroup) -ErrorAction SilentlyContinue
-    if ($null -eq $rg) {
-        New-AzResourceGroup -Name $($item.resourcegroup) -Location $($item.location) -Tag @{H="$($item.hnumber)"}
-    }
+Write-Host "Processing resource group $Name ..."
+$rg = Get-AzResourceGroup -ResourceGroupName $Name -ErrorAction SilentlyContinue
+if ($null -eq $rg) {
+    New-AzResourceGroup -Name $Name -Location $Location -Tag $Tag
 }
